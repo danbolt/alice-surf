1a360c2857bc37c53df2556eef7d15cb9ae1d2a5
Daniel Savage <dansvg@gmail.com>
2015-12-31 20:16:51 -0800

    Basic cat-collecting logic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+var Constants = {
+  Gravity : 500,
+};
+
+var Cat = function (game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'test16x16', 6);
+
+  this.game.physics.arcade.enable(this, Phaser.Physics.ARCADE);
+  this.body.allowGravity = false;
+
+  this.animations.add('cat_dance', [6, 7], 3, true);
+  this.animations.play('cat_dance');
+
+  this.game.add.existing(this);
+};
+Cat.prototype = Object.create(Phaser.Sprite.prototype);
+Cat.prototype.constructor = Cat;
+
+
 var Preload = function () {
 	//
 };
@@ -33,22 +52,68 @@ Preload.prototype.preload = function() {
 };
 Preload.prototype.create = function() {
 
+  // game logic config
+  this.level = 0; // starts at 0 for 'array indexing'
+  this.catCount = 0;
   this.game.stage.backgroundColor = '#333333';
 
-  // create a player
-  this.player = this.game.add.sprite(64, 128, 'test16x16', 3);
-  this.game.physics.arcade.enable(this.player, Phaser.Physics.ARCADE);
+  // physics config
+  this.game.physics.arcade.gravity.y = Constants.Gravity;
 
   // create map
   this.map = this.game.add.tilemap('level1');
   this.map.addTilesetImage('sheet', 'test16x16_tile');
   this.foreground = this.map.createLayer('Foreground');
+  this.foreground.resizeWorld();
   this.map.setCollisionByExclusion([0], true, this.foreground);
+
+  // create cats
+  this.cats = this.game.add.group();
+  this.map.objects.cats.forEach(function (c) {
+    var newCat = new Cat(this.game, c.x, c.y);
+
+    this.cats.addChild(newCat);
+    this.cats.addToHash(newCat);
+  }, this);
+
+  // input logic
+  this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR).onDown.add(function () {
+    this.player.body.velocity.y = -100;
+  }, this);
+
+  // ui
+  this.ui = this.game.add.group();
+  this.ui.fixedToCamera = true;
+  var levelCounter = this.game.add.bitmapText(8, 8, 'font', 'level: ' + (this.level < 9 ? '0' + (this.level + 1) : (this.level + 1)), 8);
+  this.ui.addChild(levelCounter);
+  var catCounter = this.game.add.bitmapText(8, 16, 'font', 'cats:  ' + (this.catCount < 10 ? ('0' + (this.catCount)) : (this.catCount)), 8);
+  this.catCounter = catCounter;
+  this.ui.addChild(catCounter);
+
+  // create a player
+  this.player = this.game.add.sprite(48, 128, 'test16x16', 3);
+  this.game.physics.arcade.enable(this.player, Phaser.Physics.ARCADE);
+  this.player.body.setSize(8, 8);
+  this.player.body.offset.x = 4;
+  this.player.body.offset.y = 8;
+
+  this.game.camera.follow(this.player);
 };
 Preload.prototype.update = function() {
-  this.game.physics.arcade.collide(this.player, this.foreground);
+  this.game.physics.arcade.collide(this.player, this.foreground, null, null, this);
+  this.game.physics.arcade.overlap(this.player, this.cats, undefined, function (player, cat) {
+    this.catCount++;
+    this.catCounter.text = 'cats:  ' + (this.catCount < 10 ? ('0' + (this.catCount)) : (this.catCount));
+    cat.kill();
 
-  this.player.animations.play((this.player.body.velocity.x < 0 ? 'l' : 'r') + (this.player.body.velocity.y < 0 ? 'u' : 'd'));
+    return false;
+  }, this);
+
+  this.player.body.velocity.x = 100;
+
+  if (this.player.body.onWall() || this.player.body.touching.up) {
+    this.player.kill();
+  }
 };
 
 var main = function () {
