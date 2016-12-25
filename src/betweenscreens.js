beed96e7285d36cf219e515e843a2451d9f1a744
Daniel Savage <dansvg@gmail.com>
2016-12-25 01:50:33 -0800

    Cool win message bro

--- src/betweenscreens.js
+++ src/betweenscreens.js
@@ -23,6 +23,24 @@ PreWave.prototype.create = function () {
 
 var PostWave = function () {
   this.level = 0;
+
+  this.encouragements = [
+    'nice!',
+    'good job!',
+    'the cats are safe!',
+    'well done!',
+    'good surfing!',
+    'the prez is proud',
+    'that\'s some sick surfing',
+    'yay!',
+    'quality stuff',
+    'A++',
+    'dayumnnn',
+    'cowabunga!',
+    'gnarly duuude!',
+    'killing it',
+    'hot stuff on the waves!',
+  ];
 };
 PostWave.prototype.init = function (levelNumber, catCount) {
   this.level = levelNumber;
@@ -37,15 +55,35 @@ PostWave.prototype.create = function () {
   congratsText.align = 'center';
   congratsText.anchor.set(0.5, 0.5);
 
-  var catCountLabel = this.game.add.bitmapText(this.game.width / 4, this.game.height / 2 - 16, 'font', ' cats: \n\nscore: ', 8);
+  var catCountLabel = this.game.add.bitmapText(this.game.width / 4, this.game.height / 2 - 16, 'font', '', 8);
+  var scoreValuesLabel = this.game.add.bitmapText(this.game.width / 2, this.game.height / 2 - 16, 'font', '', 8);
 
-  var scoreValuesLabel = this.game.add.bitmapText(this.game.width / 2, this.game.height / 2 - 16, 'font', this.catCount + '\n\n' + GameState.Score, 8);
-
-  var encouragementText = this.game.add.bitmapText(this.game.width / 2, this.game.height / 4 * 3 - 16, 'font', 'nice!', 8);
+  var encouragementText = this.game.add.bitmapText(this.game.width / 2, this.game.height / 4 * 3 - 16, 'font', this.encouragements[~~(this.encouragements.length * Math.random())], 8);
   encouragementText.anchor.set(0.5);
   encouragementText.align = 'center';
+  encouragementText.visible = false;
 
-  this.game.time.events.add(3000, function () {
-    this.game.state.start('PreWave', true, false, this.level + 1);
+  var encouragementAnimation = this.game.add.sprite(-32, this.game.height - 26, 'test32x32', 4);
+
+  this.game.time.events.add(650, function () {
+    catCountLabel.text +=  'cats: \n\n';
+    scoreValuesLabel.text += this.catCount + '\n\n';
+
+    this.game.time.events.add(500, function () {
+      catCountLabel.text += 'score: ';
+      scoreValuesLabel.text += GameState.Score;
+
+      this.game.time.events.add(650, function () {
+        encouragementText.visible = true;
+        var surfTween = this.game.add.tween(encouragementAnimation);
+        surfTween.to({x: this.game.width + 32}, 2200);
+        surfTween.start();
+
+        this.game.time.events.add(3000, function () {
+          this.game.state.start('PreWave', true, false, this.level + 1);
+        }, this);
+      }, this);
+
+    }, this);
   }, this);
 };
